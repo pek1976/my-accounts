7493da0e0618431bdacf505998369de41531a0a9
“Vin” <vinspecter@gmail.com>
2020-11-07 17:48:59 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'misCuentasApp';
+  // test = [{"total": 900, "name":"test", "description":"descript test..."}];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './share/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
+import { HeaderComponent } from './share/header/header.component';
+import { CuentaComponent } from './pages/cuenta/cuenta.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { CardModule } from 'card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PageNotFoundComponent,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+    CuentaComponent,
+    Grafica1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
